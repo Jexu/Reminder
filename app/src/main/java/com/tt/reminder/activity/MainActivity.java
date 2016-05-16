fc771192382e913a626e42cd1d9be14b1f8394a0
zhengguo <ttjekux@163.com>
2016-05-16 20:54:13 +0800

    modify shared_header_view, add EditTaskFragment, add animator and so on

--- app/src/main/java/com/tt/reminder/activity/MainActivity.java
+++ app/src/main/java/com/tt/reminder/activity/MainActivity.java
@@ -1,22 +1,26 @@
 package com.tt.reminder.activity;
 
-import android.support.v4.app.FragmentManager;
-import android.support.v4.app.FragmentTransaction;
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.FrameLayout;
-import android.widget.TextView;
+import android.widget.Toast;
 
 import com.tt.reminder.R;
-import com.tt.reminder.fragment.TasksContaintWithDrawerViewFragment;
-import com.tt.sharedutils.DeviceUtil;
+import com.tt.reminder.fragment.TasksContainWithDrawerViewFragment;
+import com.tt.sharedbaseclass.listener.OnFragmentInteractionListener;
 
-public class MainActivity extends AppCompatActivity implements TasksContaintWithDrawerViewFragment.OnFragmentInteractionListener{
+public class MainActivity extends AppCompatActivity implements OnFragmentInteractionListener {
 
+    private static long INTERVAL_OF_DOUBLE_BACK_PRESSED_DOUBLE_CLICK = 500;
+    private static int IS_SEED_FRAGMENT = 0;
+    private static int mFragmentCounter = 0;
     private FrameLayout mMainActivityFrameLayout;
-    private FragmentManager mFragmentManager;
-    private FragmentTransaction mTransaction;
-    private TasksContaintWithDrawerViewFragment mTasksContaintWithDrawerViewFragment;
+    private TasksContainWithDrawerViewFragment mTasksContainWithDrawerViewFragment;
+    private long mFirstBackPressedTime = 0;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -25,9 +29,6 @@ public class MainActivity extends AppCompatActivity implements TasksContaintWith
             //do something
         }
         mMainActivityFrameLayout = (FrameLayout) findViewById(R.id.main_activity_frame_layout);
-        mFragmentManager = getSupportFragmentManager();
-        mTransaction = mFragmentManager.beginTransaction();
-
         onCreated();
     }
 
@@ -37,14 +38,40 @@ public class MainActivity extends AppCompatActivity implements TasksContaintWith
 
     private void initMainActivityFragment() {
 
-        mTasksContaintWithDrawerViewFragment = new TasksContaintWithDrawerViewFragment();
-        mTransaction.add( R.id.main_activity_frame_layout, mTasksContaintWithDrawerViewFragment );
-        mTransaction.addToBackStack(null);
-        mTransaction.commit();
+        FragmentManager mFragmentManager = getFragmentManager();
+        FragmentTransaction transaction = mFragmentManager.beginTransaction();
+        mTasksContainWithDrawerViewFragment = TasksContainWithDrawerViewFragment.newInstance();
+        transaction.add(R.id.main_activity_frame_layout, mTasksContainWithDrawerViewFragment);
+        transaction.addToBackStack(null);
+        transaction.commit();
     }
 
     @Override
-    public void onFragmentInteraction(Bundle bundle) {
+    public void onBackPressed() {
+        if (mFragmentCounter == IS_SEED_FRAGMENT) {
+            if (mFirstBackPressedTime == 0 || (System.currentTimeMillis() - mFirstBackPressedTime) >= INTERVAL_OF_DOUBLE_BACK_PRESSED_DOUBLE_CLICK) {
+                mFirstBackPressedTime = System.currentTimeMillis();
+                Toast.makeText(this, R.string.toast_double_click_to_exit, Toast.LENGTH_SHORT).show();
+            } else {
+                finish();
+            }
+        } else {
+            mFragmentCounter--;
+            super.onBackPressed();
+        }
+    }
+
+    public static void navigateTo(android.app.Fragment fragment, android.app.FragmentManager fragmentManager) {
+        android.app.FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        //fragmentTransaction.setCustomAnimations(R.animator.fragment_slide_in_bottom,R.animator.fragment_slide_out_top);
+        fragmentTransaction.replace(R.id.main_activity_frame_layout, fragment);
+        fragmentTransaction.addToBackStack(null);
+        fragmentTransaction.commit();
+        mFragmentCounter++;
+    }
+
+    @Override
+    public void onFragmentInteraction(Uri uri) {
 
     }
 }
---
+++ sharedbaseclass/src/main/java/com/tt/sharedbaseclass/listener/OnFragmentInteractionListener.java
@@ -0,0 +1,11 @@
+package com.tt.sharedbaseclass.listener;
+
+import android.net.Uri;
+
+/**
+ * Created by zhengguo on 2016/5/16.
+ */
+public interface OnFragmentInteractionListener {
+    // TODO: Update argument type and name
+    void onFragmentInteraction(Uri uri);
+}
